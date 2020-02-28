46a35acb29d4d36d124f7762de27a2e161730708
github username <github email address>
2020-02-28 14:49:00 -0500

    first commit

---
+++ Block.js
@@ -0,0 +1,21 @@
+
+class Block {
+    constructor(timestamp,lasthash ,hash,data) {
+        this.timestamp = timestamp ;
+        this.lastHash = lastHash ;
+        this.Hash = Hash ;
+        this.data = data ; 
+
+    }
+
+    toString() {
+        return `Block -
+        Timestamp: ${this.timestamp}
+        lastHash : ${this.lastHash.substring(0,10)}
+        Hash     : ${this.Hash.substring(0,10)}
+        Data     : ${this.data}` ; 
+    }
+}  
+
+
+module.exports = Block 
